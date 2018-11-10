6f975158a624e2e03013ad0c244343e52cc085d6
mihailm94 <mihailmilchev@gmail.com>
2018-11-10 12:47:05 +0100

    Gmaps API key in gradle.settings

--- app/src/main/java/com/appspot/airpeepee/airpeepee/MapsActivity.java
+++ app/src/main/java/com/appspot/airpeepee/airpeepee/MapsActivity.java
@@ -1,7 +1,13 @@
 package com.appspot.airpeepee.airpeepee;
 
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +15,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.api.services.people.v1.model.Address;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
