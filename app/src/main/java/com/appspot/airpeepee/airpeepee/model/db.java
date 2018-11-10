241b4c7900d33afc8a9f00c28665ca5350f8a252
mihailm94 <mihailmilchev@gmail.com>
2018-11-10 12:39:38 +0100

    google maps api key

--- app/src/main/java/com/appspot/airpeepee/airpeepee/model/db.java
+++ app/src/main/java/com/appspot/airpeepee/airpeepee/model/db.java
@@ -65,24 +65,25 @@ public class db {
 
     public db(String toilet, String plz) {
 
-        // Get a reference to our posts
+        // Get a reference to our toilets
         final FirebaseDatabase firedb = FirebaseDatabase.getInstance();
-        DatabaseReference ref = firedb.getReference("/toilet");
+        DatabaseReference toiletRef = firedb.getReference("/toilet");
 
         /* Attach a listener to read the data at our posts reference */
-        ref.addValueEventListener(new
+        toiletRef.addValueEventListener(new
 
               ValueEventListener() {
                   @Override
                   public void onDataChange(DataSnapshot dataSnapshot) {
-                      db database = dataSnapshot.getValue(db.class);
+                      Toilet toilet = dataSnapshot.getValue(Toilet.class);
 
-                      System.out.println(dataSnapshot.getKey()); //das ist die Toilet collection (table toilet)
-                      System.out.println(dataSnapshot.getValue());  // das ist das Value von das Table. In diesem Fall - das ganze JSON
+                      //Log.d("toilet:", String.valueOf(toilet));
+                      System.out.println(toilet);
+                      //System.out.println(dataSnapshot.getKey()); //das ist die Toilet collection (table toilet)
+                      //System.out.println(dataSnapshot.getValue());  // das ist das Value von das Table. In diesem Fall - das ganze JSON
 
-                      Log.d("db:", database.toString());
+                      //Log.d("db:", toile.toString());
                   }
-
 
                   @Override
                   public void onCancelled(DatabaseError databaseError) {
@@ -91,4 +92,5 @@ public class db {
               });
 
     }
+
 }
