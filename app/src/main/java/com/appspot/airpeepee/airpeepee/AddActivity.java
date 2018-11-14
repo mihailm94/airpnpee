d925c7907e437feb27281011badcd71a9ba7ba7a
osamax2 <osobaji@gmail.com>
2018-11-14 01:33:04 +0100

    NoTitleBar in Add Activity

--- app/src/main/java/com/appspot/airpeepee/airpeepee/AddActivity.java
+++ app/src/main/java/com/appspot/airpeepee/airpeepee/AddActivity.java
@@ -37,7 +37,9 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
         mDatabase =  new db();
