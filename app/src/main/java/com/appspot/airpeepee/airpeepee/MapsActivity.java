0cce4a4d80649b6fa94a9c2feda9aa09fae695d5
osamax2 <osobaji@gmail.com>
2018-12-02 16:00:16 +0100

    fix menu in mapActifity

--- app/src/main/java/com/appspot/airpeepee/airpeepee/MapsActivity.java
+++ app/src/main/java/com/appspot/airpeepee/airpeepee/MapsActivity.java
@@ -122,14 +122,6 @@ EditToiletActivity.NoticeDialogListener
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
          setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
