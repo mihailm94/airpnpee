c6587759d09b5f45a36d9994d2e5653c9ca1ce07
ElnazBezdoodeh <s0560900@htw-berlin.de>
2018-12-04 10:46:09 +0100

    Setting details

--- app/src/main/java/com/appspot/airpeepee/airpeepee/SettingsActivity.java
+++ app/src/main/java/com/appspot/airpeepee/airpeepee/SettingsActivity.java
@@ -141,7 +141,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"contact@androidhive.info"});
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"s0560900@htw-berlin.de"});
         intent.putExtra(Intent.EXTRA_SUBJECT, "Query from android app");
         intent.putExtra(Intent.EXTRA_TEXT, body);
         context.startActivity(Intent.createChooser(intent, context.getString(R.string.choose_email_client)));
